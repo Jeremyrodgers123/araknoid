4fc691708b743691b9fd57de21f6b91c1ae81c85
jeremyrodgers123 <jeremy.rodgers88@gmail.com>
2018-09-17 16:26:56 -0600

    working bar

---
+++ bar.cpp
@@ -0,0 +1,8 @@
+//
+//  bar.cpp
+//  arkanoid
+//
+//  Created by Jeremy Rodgers on 9/17/18.
+//
+
+#include "bar.hpp"
---
+++ bar.hpp
@@ -0,0 +1,13 @@
+//
+//  bar.hpp
+//  arkanoid
+//
+//  Created by Jeremy Rodgers on 9/17/18.
+//
+
+#ifndef bar_hpp
+#define bar_hpp
+
+#include <stdio.h>
+
+#endif /* bar_hpp */
